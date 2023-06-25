fe3ae54042745a60121733b05782e1fa86171342
Sarah Meyer <sarah@macbook-pro-von-sarah.home>
2023-06-25 21:16:14 +0200

    fixed version

--- translation.rb
+++ translation.rb
@@ -1,11 +1,10 @@
 
 def translate(word)
   translations = {dog:"Hund", cat: "Katze", sheep: "Schaf", cow: "Kuh"}
-  if translations.has_key?(word)
-    puts "The translated word is in German is #{translations[word]}"
-    
+  if translations.has_key?(word) 
+    puts "The translated word in German is #{translations[word]}"
   elsif translations.key(word.to_s.capitalize)
-    puts "The translated word is in English is #{translations.key(word.to_s.capitalize)}."
+    puts "The translated word in English is #{translations.key(word.to_s.capitalize)}."
   else
     puts "I’m sorry, I can’t translate that."
   end
