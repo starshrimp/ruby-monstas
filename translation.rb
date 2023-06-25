84d1f7b51c834dbcf404fa0edf528742d4ea767d
Sarah Meyer <sarah@macbook-pro-von-sarah.home>
2023-06-25 21:06:53 +0200

    added a new exercise

---
+++ translation.rb
@@ -0,0 +1,15 @@
+
+def translate(word)
+  translations = {dog:"Hund", cat: "Katze", sheep: "Schaf", cow: "Kuh"}
+  if translations.has_key?(word)
+    puts "The translated word is in German is #{translations[word]}"
+    
+  elsif translations.key(word.to_s.capitalize)
+    puts "The translated word is in English is #{translations.key(word.to_s.capitalize)}."
+  else
+    puts "I’m sorry, I can’t translate that."
+  end
+end
+
+input = gets.chomp.to_sym
+translate(input)
